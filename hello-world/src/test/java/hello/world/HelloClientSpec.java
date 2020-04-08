72e702230d8a85e45b2e734d58a89c1a5f56dd98
Parveen Parmar <parveenparmar03@gmail.com>
2020-04-08 21:43:13 +0530

    [P3arl] Hello world example

---
+++ hello-world/src/test/java/hello/world/HelloClientSpec.java
@@ -0,0 +1,20 @@
+package hello.world;
+
+import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MicronautTest
+public class HelloClientSpec  {
+
+    @Inject
+    HelloClient client;
+
+    @Test
+    public void testHelloWorldResponse(){
+        assertEquals("Hello World", client.hello().blockingGet());
+    }
+}
